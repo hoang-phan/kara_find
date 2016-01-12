080d7f8643e727f55f05a8cb1fbd006b236f1ce0
hoangphanea <hoang.phan@eastagile.com>
2016-01-12 14:13:49 +0700

    [f] Add basic UI

--- app/src/main/java/vn/hoangphan/karafind/SearchActivity.java
+++ app/src/main/java/vn/hoangphan/karafind/SearchActivity.java
@@ -1,13 +1,36 @@
 package vn.hoangphan.karafind;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class SearchActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import vn.hoangphan.karafind.adapters.SongsAdapter;
+import vn.hoangphan.karafind.models.Song;
+
+public class SearchActivity extends Activity {
+    private RecyclerView mRvSongs;
+    private SongsAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        mRvSongs = (RecyclerView)findViewById(R.id.rv_songs);
+
+        mAdapter = new SongsAdapter();
+        mRvSongs.setAdapter(mAdapter);
+        mRvSongs.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Song> songs = new ArrayList<>();
+        songs.add(new Song("Dau mua", "Mot con mua di qua...", "Trung Quan Idol", "54433", 53, true));
+        songs.add(new Song("Vet mua", "Con mua, da xoa het nhung ngay yeu qua...", "Vu Cat Tuong", "53232", 54, false));
+        mAdapter.setSongs(songs);
+        mAdapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/vn/hoangphan/karafind/models/Song.java
@@ -0,0 +1,71 @@
+package vn.hoangphan.karafind.models;
+
+/**
+ * Created by eastagile-tc on 1/12/16.
+ */
+public class Song {
+    private String name;
+    private String lyric;
+    private String author;
+    private String id;
+    private boolean favorited;
+
+    private int vol;
+
+    public Song(String name, String lyric, String author, String id, int vol, boolean favorited) {
+        this.name = name;
+        this.lyric = lyric;
+        this.author = author;
+        this.vol = vol;
+        this.id = id;
+        this.favorited = favorited;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLyric() {
+        return lyric;
+    }
+
+    public void setLyric(String lyric) {
+        this.lyric = lyric;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getVol() {
+        return vol;
+    }
+
+    public void setVol(int vol) {
+        this.vol = vol;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isFavorited() {
+        return favorited;
+    }
+
+    public void setFavorited(boolean favorited) {
+        this.favorited = favorited;
+    }
+}
